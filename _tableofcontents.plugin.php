2d5a7fe23078a7a8697a8a93d2a2e3f59b00c9ab
Han Jinsong <jshan@ybu.edu.cn>
2017-06-18 15:43:14 +0800

    1st commit
    1st commit

---
+++ _tableofcontents.plugin.php
@@ -0,0 +1,136 @@
+<?php
+
+/**
+ * This file implements the Table of content plugin.
+ *
+ * For the most recent and complete Plugin API documentation
+ * see {@link Plugin} in ../inc/plugins/_plugin.class.php.
+ *
+ * This file is part of the evoCore framework - {@link http://evocore.net/}
+ * See also {@link https://github.com/b2evolution/b2evolution}.
+ *
+ * @license GNU GPL v2 - {@link http://b2evolution.net/about/gnu-gpl-license}
+ *
+ * @copyright (c)2003-2016 by Francois Planque - {@link http://fplanque.com/}
+ * Parts of this file are copyright (c)2004-2006 by Daniel HAHLER - {@link http://thequod.de/contact}.
+ *
+ * @package plugins
+ */
+if (!defined('EVO_MAIN_INIT'))
+	die('Please, do not access this page directly.');
+
+/**
+ * Table of content Plugin
+ *
+ * This plugin responds to virtually all possible plugin events :P
+ *
+ * @package plugins
+ */
+class tableofcontents_plugin extends Plugin {
+
+	/**
+	 * Variables below MUST be overriden by plugin implementations,
+	 * either in the subclass declaration or in the subclass constructor.
+	 */
+	var $name = 'Table of contents';
+	var $code = 'evo_tableofcontent';
+	var $group = 'rendering';
+	var $short_desc;
+	var $long_desc;
+	var $priority = 99;
+	var $number_of_installs = 1;
+	var $version = '0.1';
+
+	/**
+	 * Init
+	 *
+	 * This gets called after a plugin has been registered/instantiated.
+	 */
+	function PluginInit(& $params) {
+		$this->short_desc = T_('Table of post contents plugin');
+		$this->long_desc = T_('This would make the table of the post contents automatically by the html tag h1 to h6.');
+	}
+	
+		/**
+	 * Define the GLOBAL settings of the plugin here. These can then be edited in the backoffice in System > Plugins.
+	 *
+	 * @see Plugin::GetDefaultSettings()
+	 * @param array Associative array of parameters (since v1.9).
+	 *    'for_editing': true, if the settings get queried for editing;
+	 *                   false, if they get queried for instantiating {@link Plugin::$Settings}.
+	 * @return array see {@link Plugin::GetDefaultSettings()}.
+	 * The array to be returned should define the names of the settings as keys (max length is 30 chars)
+	 * and assign an array with the following keys to them (only 'label' is required):
+	 */
+	function GetDefaultSettings( & $params )
+	{
+		$r = array(
+			'list_style' => array(
+				'label' => 'Select a list style',
+				'defaultvalue' => 'order',
+				'type' => 'select',
+				'options' => array( 'order' => 'Order List', 'unorder' => 'Unordered list' ),
+				'note' => 'This is the style of the content table list',
+			)
+		);
+		return $r;
+	}
+
+	/**
+	 * Define here default collection/blog settings that are to be made available in the backoffice.
+	 *
+	 * @param array Associative array of parameters.
+	 * @return array See {@link Plugin::get_coll_setting_definitions()}.
+	 */
+	function get_coll_setting_definitions(& $params) {
+		$default_params = array_merge($params, array(
+		    'default_post_rendering' => 'opt-out',
+		    'default_comment_rendering' => 'never'
+		));
+		return parent::get_coll_setting_definitions($default_params);
+	}
+
+	/**
+	 * Event handler: Called at the beginning of the skin's HTML HEAD section.
+	 *
+	 * Use this to add any HTML HEAD lines (like CSS styles or links to resource files (CSS, JavaScript, ..)).
+	 *
+	 * @see Plugin::SkinBeginHtmlHead()
+	 * @param array Associative array of parameters
+	 */
+	function SkinBeginHtmlHead(& $params) {
+		$this->require_js('js/toc.js');
+		$this->require_js('js/tableofcontent.js');
+	}
+
+	/**
+	 * Event handler: Called when rendering item/post contents as HTML. (CACHED)
+	 *
+	 * The rendered content will be *cached* and the cached content will be reused on subsequent displays.
+	 * Use {@link DisplayItemAsHtml()} instead if you want to do rendering at display time.
+	 *
+	 * Note: You have to change $params['data'] (which gets passed by reference).
+	 *
+	 * @see Plugin::DisplayItemAsHtml()
+	 * @param array Associative array of parameters
+	 *   - 'data': the data (by reference). You probably want to modify this.
+	 *   - 'format': see {@link format_to_output()}. Only 'htmlbody' and 'entityencoded' will arrive here.
+	 *   - 'Item': the {@link Item} object which gets rendered.
+	 *   - 'view_type': What part of a post are we displaying: 'teaser', 'extension' or 'full'
+	 * @return boolean Have we changed something?
+	 */
+	function RenderItemAsHtml(& $params) {
+		global $Item;
+		$page_url = is_object($Item) ? $Item->get_permanent_url() : '';
+		$list_style = $this->Settings->get('list_style');
+		$content_id = 'item_' . (isset($Item->ID) ? $Item->ID : '').' .evo_post__full_text';
+		$script = '<script>$(document).ready(function() {new Toc({target: "h1",content: "#'.$content_id.'",depth: 6, page_url: "'.$page_url.'", list_style:"'.$list_style.'"});});</script>';
+		$params['data'] .=  $script;
+
+		return true;
+	}
+
+}
+
+
+?>
---
+++ js/toc.js
@@ -0,0 +1,212 @@
+/*
+ * This basic js comes from https://github.com/PebbleRoad/auto-table-of-contents author:rmdort
+ * Edited by haharen for b2evolution.
+ */
+(function (window) {
+
+    'use strict'
+
+    /**
+     * Shims
+     */
+
+    function getTextContent(elem) {
+        if ((elem.textContent) && (typeof (elem.textContent) != "undefined")) {
+            return elem.textContent;
+        } else {
+            return elem.innerText;
+        }
+    }
+
+    /*
+     Options
+     */
+
+    var defaultOptions = {
+        content: 'body',
+        start: 'h2',
+        depth: 6,
+        klass: 'list--toc',
+        appendTo: ''
+    };
+
+
+    /**
+     * Constructor
+     */
+
+    function Toc(options) {
+
+        this.options = this.extend({}, defaultOptions, options);
+
+        this.obj = document.querySelector(this.options.content);
+
+        this.options.start = this.options.start.substr(1);
+
+        this.traverse();
+    }
+
+    function get_list_tag(list_style) {
+        var value = Array();
+        if (list_style == "order") {
+            value[0] = "<ol>";
+            value[1] = "</ol>";
+        } else {
+            value[0] = "<ul>";
+            value[1] = "</ul>";
+        }
+        return value;
+    }
+
+    /**
+     * Traverse
+     */
+
+    Toc.prototype.traverse = function () {
+
+        var html = '',
+                tagNumber = 0,
+                txt = '',
+                id = '',
+                page_url = this.options.page_url,
+                list_style = this.options.list_style,
+                previous = this.options.start - 1,
+                start = this.options.start,
+                depth = this.options.depth,
+                i = 0,
+                srcTags = 'h' + this.options.start,
+                element = '';
+
+        var arr_list_tag = get_list_tag(list_style);
+
+        while (depth > 1) {
+            start++;
+            srcTags = srcTags + ", h" + start,
+                    depth--;
+        }
+
+
+        var found = this.obj.querySelectorAll(srcTags);
+        if (found.length >= 1) {
+            html += '<div style="clear:both"></div>';
+            html += '<div style="border:solid 1px #ccc; background:#eee; float:left; min-width:200px;padding:4px 10px;">';
+            html += '<p style="text-align:right;margin:0;"><span style="float:left;">Table of Contents<a title="System would build the table of the content by the html tag h1 to h6">(?)</a></span><a href="#" onclick="javascript:return openExpand(this);" title="Expand">[+]</a></p>';
+            html += '<div style="display:none;line-height:160%;">';
+        }
+        for (var j = 0; j < found.length; j++) {
+
+            element = found[j];
+
+            /* Get the tag number of the element - 2, 3, 4, 5, 6 */
+            tagNumber = element.tagName.substr(1);
+
+            /* Text of the element */
+
+            txt = getTextContent(element);
+            var tit = txt.replace(/^\d+[.、\s]+/g, '');
+            tit = txt.replace(/[^a-zA-Z0-9_\-\s\u4e00-\u9fa5]+/g, '');
+
+            /* Get and Set ID Attribute */
+            id = element.getAttribute('id') || tit;
+
+            //element.setAttribute("id", id);
+            $(element).before('<a name="' + id + '" style="position: relative;"><span style="position: absolute;display:block; width:100px; height:50px;  top: -30px;  "></span></a>');
+            $(element).append('<span style="position: absolute;display:block; width:100px; height:50px;  top: -30px;  "></span>');
+            $("#" + id).css("position", "relative");
+
+            /* Build UL */
+            if (tit != null && tit.replace(/(^\s*)|(\s*$)/g, "") != "" && tit.length < 100) {
+                switch (true) {
+                    case (tagNumber > previous):
+                        html += arr_list_tag[0] + "<li class=\"toc-" + id + "\"><a href=\"" + page_url + "#" + id + "\">" + tit + "</a>";
+                        previous = tagNumber;
+                        break;
+
+                    case (tagNumber == previous):
+                        html += "</li><li class=\"toc-" + id + "\"><a href=\"" + page_url + "#" + id + "\">" + tit + "</a>";
+                        break;
+
+                    case (tagNumber < previous):
+                        while (tagNumber != previous) {
+                            html += arr_list_tag[1] + "</li>";
+                            previous--;
+                        }
+                        html += "<li class=\"toc-" + id + "\"><a href=\"" + page_url + "#" + id + "\">" + tit + "</a>";
+                        break;
+                }
+
+            }
+        }
+
+
+        /* corrects our last item, because it may have some opened ul's */
+        if (tagNumber > this.options.start) {
+            while (tagNumber != this.options.start) {
+                html += arr_list_tag[1];
+                tagNumber--;
+            }
+        }
+        if (found.length >= 1) {
+            html += '</div></div>';
+            html += '<div style="clear:both;margin-bottom:15px"></div>';
+        }
+
+        var fhtml = document.createElement('div');
+        fhtml.className = this.options.klass;
+        fhtml.innerHTML = html;
+
+        if (this.options.appendTo) {
+            var appendto = document.querySelector(this.options.appendTo)
+
+            appendto.insertBefore(fhtml, appendto.firstChild);
+
+        } else {
+            this.obj.insertBefore(fhtml, this.obj.firstChild);
+        }
+
+    };
+
+
+    /**
+     * Shims
+     */
+
+    function getTextContent(elem) {
+        if ((elem.textContent) && (typeof (elem.textContent) != "undefined")) {
+            return elem.textContent;
+        } else {
+            return elem.innerText;
+        }
+    }
+
+    /*
+     * Extend
+     */
+
+    Toc.prototype.extend = function () {
+        if (arguments.length > 1) {
+            var master = arguments[0];
+            for (var i = 1, l = arguments.length; i < l; i++) {
+                var object = arguments[i];
+                for (var key in object) {
+                    master[key] = object[key];
+                }
+            }
+        }
+        return master;
+    };
+
+
+    return window.Toc = Toc;
+
+})(window, undefined)
+
+function openExpand(e) {
+    if (e.innerHTML == '[+]') {
+        $(e).attr('title', 'Shrink').html('[-]').parent().next().show();
+    } else {
+        $(e).attr('title', 'Expand').html('[+]').parent().next().hide();
+    }
+    e.blur();
+    return false;
+}
